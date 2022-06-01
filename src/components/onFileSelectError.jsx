da8ea809f992e0229458b42badf08554482e3055
dmta9796 <dmta9796@colorado.edu>
2022-06-01 09:49:45 -0600

    small experiment with file form and hooks

---
+++ src/components/onFileSelectError.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const onFileSelectError = ({error}) => {
+    return (
+        <div className="file-uploader">
+            <p>failure</p>
+        </div>
+    )
+}
+export default onFileSelectError;
---
+++ src/components/onFileSelectSuccess.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const onFileSelectSuccess = ({file}) => {
+    return (
+        <div className="file-uploader">
+            <p>success</p>
+        </div>
+
+    )
+
+}
+export default onFileSelectSuccess;
