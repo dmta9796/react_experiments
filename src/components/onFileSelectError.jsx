d7400d0ddc2e7860afe0314f0f579b3aa7bc19a4
dmta9796 <dmta9796@colorado.edu>
2022-06-02 11:24:38 -0600

    image load experiment

--- src/components/onFileSelectError.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-const onFileSelectError = ({error}) => {
-    return (
-        <div className="file-uploader">
-            <p>failure</p>
-        </div>
-    )
-}
-export default onFileSelectError;
