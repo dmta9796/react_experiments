71ce0ee82df4f62c528bdbbf26c777c7c8e96c06
dmta9796 <dmta9796@colorado.edu>
2022-06-14 12:22:59 -0600

    use effect to fetch data

--- src/components/ArcWrapper.jsx
+++ src/components/ArcWrapper.jsx
@@ -1,12 +1,30 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ArcComponent from './ArcComponent';
 import { Box } from "@material-ui/core";
 
 
 function ArcWrapper() {
+    useEffect(() => {
+      const url = "https://api.weather.gov/gridpoints/TOP/31,80"
+    
+      const fetchData = async () => {
+        try {
+          const response = await fetch(url);
+          const json = await response.json();
+          console.log(json);
+        } catch (error) {
+          console.log("error", error);
+        }
+      };
+    
+      fetchData();
+    }, []);
+
+
+
     const svgWidth = 150;
     const arcWidth = 12;
-    const [progressPercentage, setProgressPercentage] = useState(50);
+    const [progressPercentage, setProgressPercentage] = useState(100);
     const colorIndicator = "red"; //useColorIndication(progressPercentage);
     function valuetext(value) {
       return `${value}°C`;
